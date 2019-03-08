6e3eba00b54b0a0a0d7891577915fd68e56b1280
Rodrigo Hurtado Molero <matrix00siete@hotmail.com>
2019-03-08 16:58:43 -0500

    Repairs, messages, and listings UI.

---
+++ Desktop/Sheridan/Semester7/Capstone/project/RoomREP/app/src/main/java/com/example/ryan/roomrep/TenantRepairFragment.java
@@ -0,0 +1,31 @@
+package com.example.ryan.roomrep;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class TenantRepairFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //Change
+        // Inflate the layout for this fragment
+        View view  =  inflater.inflate(R.layout.fragment_tenant_repair, container, false);
+
+
+
+
+
+        return view;
+    }
+
+
+}
