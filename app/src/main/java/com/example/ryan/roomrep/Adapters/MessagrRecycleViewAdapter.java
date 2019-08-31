11d9531fbf99f8bf6d6bfbe836f5658e0da2a123
Ziheng He <hezih@sheridancollege.ca>
2019-08-31 23:23:04 +0800

    Real time Messaging system implemented

--- app/src/main/java/com/example/ryan/roomrep/Adapters/MessagrRecycleViewAdapter.java
+++ app/src/main/java/com/example/ryan/roomrep/Adapters/MessagrRecycleViewAdapter.java
@@ -15,16 +15,41 @@ import java.util.List;
 
 public class MessagrRecycleViewAdapter extends RecyclerView.Adapter<MessagrRecycleViewAdapter.ViewHolder>{
 
-    private List<String> mData;
+    private static final String TAG = "MessagrRecyclerView";
+    private List<ChatMessage> mData;
+    private Context context;
     private LayoutInflater mInflater;
-    private ItemClickListener mClickListener;
-    private ChatMessage infoMessage = new ChatMessage();
 
 
     // data is passed into the constructor
-    public MessagrRecycleViewAdapter(Context context, List<String> data) {
+    public MessagrRecycleViewAdapter(Context context, List<ChatMessage> data) {
+        this.context=context;
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
+
+    }
+
+
+
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+
+        //String animal = mData.get(position);
+        //holder.myTextView.setText(animal);
+        //String name = "Ryan";
+
+        //holder.userName.setText(name);
+        //holder.time.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+        //        infoMessage.getMessageTime()));
+
+        ChatMessage chatMessage = mData.get(position);
+        holder.myTextView.setText(chatMessage.getMessageText());
+        holder.userName.setText(chatMessage.getMessageUser());
+        holder.time.setText(chatMessage.getMessageTime());
+
+
     }
 
     // inflates the row layout from xml when needed
@@ -35,29 +60,14 @@ public class MessagrRecycleViewAdapter extends RecyclerView.Adapter<MessagrRecyc
         return new ViewHolder(view);
     }
 
-
-    // binds the data to the TextView in each row
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-
-        String animal = mData.get(position);
-        holder.myTextView.setText(animal);
-        String name = "Ryan";
-
-        holder.userName.setText(name);
-        holder.time.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
-                infoMessage.getMessageTime()));
-    }
-
-    // total number of rows
     @Override
     public int getItemCount() {
         return mData.size();
     }
 
-
-    // stores and recycles views as they are scrolled off screen
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class ViewHolder extends RecyclerView.ViewHolder
+            //implements View.OnClickListener
+    {
         TextView myTextView;
         TextView userName;
         TextView time;
@@ -68,28 +78,31 @@ public class MessagrRecycleViewAdapter extends RecyclerView.Adapter<MessagrRecyc
             myTextView = itemView.findViewById(R.id.message_text);
             userName = itemView.findViewById(R.id.message_user);
             time = itemView.findViewById(R.id.message_time);
-            itemView.setOnClickListener(this);
+            //itemView.setOnClickListener(this);
         }
+//        public MessagrRecycleViewAdapter(List<ChatMessage> www){
+//            mData = www;
+//        }
 
-        @Override
-        public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
-        }
+        // @Override
+        //public void onClick(View view) {
+        //  if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        //}
     }
 
     // convenience method for getting data at click position
-    String getItem(int id) {
-        return mData.get(id);
-    }
+    //String getItem(int id) {
+     //   return mData.get(id);
+    //}
 
     // allows clicks events to be caught
-    void setClickListener(ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
-    }
+    //void setClickListener(ItemClickListener itemClickListener) {
+     //   this.mClickListener = itemClickListener;
+    //}
 
     // parent activity will implement this method to respond to click events
-    public interface ItemClickListener {
-        void onItemClick(View view, int position);
-    }
+    //public interface ItemClickListener {
+     //   void onItemClick(View view, int position);
+    //}
 
 }
--- app/src/main/java/com/example/ryan/roomrep/TenantFragments/MessagRFragment.java
+++ app/src/main/java/com/example/ryan/roomrep/TenantFragments/MessagRFragment.java
@@ -16,7 +16,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ryan.roomrep.Adapters.MessagrRecycleViewAdapter;
+import com.example.ryan.roomrep.Adapters.RepairRecyclerViewAdapter;
 import com.example.ryan.roomrep.Classes.ChatMessage;
+import com.example.ryan.roomrep.Classes.Repair;
 import com.example.ryan.roomrep.MainActivityTenant;
 import com.example.ryan.roomrep.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,109 +27,244 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 import static android.support.constraint.Constraints.TAG;
 
 
-public class MessagRFragment extends Fragment implements MessagrRecycleViewAdapter.ItemClickListener{
-    public static final String MESSAGE_KEY = "message";
-    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("communication/message");
+public class MessagRFragment extends Fragment
+        //implements MessagrRecycleViewAdapter.ItemClickListener
+        {
+   // public static final String MESSAGE_KEY = "message";
+    //private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("communication/message");
     //FirebaseFirestore mDocRef = FirebaseFirestore.getInstance();
-    MessagrRecycleViewAdapter adapter;
+   MessagrRecycleViewAdapter adapter;
+   //private static final String TAG = "MessagrRecyclerView";
     RecyclerView messageList;
-    ArrayList<String> messages;
+    ChatMessage messageSend;
+    //ArrayList<String> messages;
+    ArrayList<ChatMessage> infMessages = new ArrayList<>();
     Button send;
     EditText editText;
-    TextView userName;
-    TextView time;
-    ChatMessage inputInfo;
+    //TextView userName;
+    //TextView time;
+    //ChatMessage inputInfo;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+            @Override
+            public void onStart() {
+                super.onStart();
+                db.collection("Test").addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+                        infMessages.clear();
+                        //messageList.scrollToPosition ( infMessages.size ()-1 );
+                        getMessageFromDataBase();
 
-    @Override
+                    }
+                });
+            }
+
+            @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
 
 
 
-
+        //messages = new ArrayList<>();
+        //infMessages = new ArrayList<>();
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_messag_r, container, false);
-        messages = new ArrayList<>();
 
-        messageList = view.findViewById(R.id.rcyMessages);
+
+
         send = view.findViewById(R.id.sendButton);
-        send.setOnClickListener(onSend);
+        //send.setOnClickListener(onSend);
         editText = view.findViewById(R.id.editText);
         //userName = view.findViewById(R.id.message_user);
         //time = view.findViewById(R.id.message_time);
+        messageList = view.findViewById(R.id.recyclerRepairsView);
         messageList.setLayoutManager(new LinearLayoutManager(getActivity()));
-        adapter = new MessagrRecycleViewAdapter(getActivity(), messages);
-
+        //adapter = new MessagrRecycleViewAdapter(getActivity(), messages);
+        //adapter = new MessagrRecycleViewAdapter(getActivity(),infMessages);
         ////adapter.setClickListener(this);
-        messageList.setAdapter(adapter);
+
+
         messageList.setItemAnimator(new DefaultItemAnimator());
+
         //DocumentReference docRef = db.collection("cities").document("BJ");
-        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                //City city = documentSnapshot.toObject(City.class);
-                inputInfo = documentSnapshot.toObject(ChatMessage.class);
+        //mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+         //   @Override
+         //   public void onSuccess(DocumentSnapshot documentSnapshot) {
+          //      //City city = documentSnapshot.toObject(City.class);
+           //     inputInfo = documentSnapshot.toObject(ChatMessage.class);
                 //Toast.makeText(getActivity(), inputInfo.getMessageUser(), Toast.LENGTH_LONG).show();
-                messages.add(inputInfo.getMessageText());
+            //    messages.add(inputInfo.getMessageText());
                 //userName.setText("Andy");
-                adapter.notifyDataSetChanged();
+              //  adapter.notifyDataSetChanged();
+            //}
+        //});
+        //infMessages.clear();
+        //getMessageFromDataBase();
+
+
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                            if(!editText.getText().toString().equals("")){
+                                //Toast.makeText(getActivity(), editText.getText().toString(), Toast.LENGTH_SHORT).show();
+                messageSend= new ChatMessage();
+
+                messageSend.setMessageUser("FATHER");
+                messageSend.setMessageText(editText.getText().toString());
+
+                String sendDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+                DateFormat sendTime = new SimpleDateFormat("hh:mm:ss ");
+                String formatTime = sendTime.format(new Date());
+                messageSend.setMessageTime(sendDate + " " + formatTime);
+
+
+                sendDataTodataBase();
+
+
+                editText.setText("");
+            }else{
+                Toast.makeText(getActivity(), "message can not be empty", Toast.LENGTH_SHORT).show();
             }
-        });
-
-
-        return view;
-
-
-
-
-    }
-
-    public void saveMessage(){
-        String message = editText.getText().toString();
-        ChatMessage infoMessage = new ChatMessage(message,"Ryan");
-        if (message.isEmpty()){return;}
-        mDocRef.set(infoMessage);
-    }
-
-    @Override
-    public void onItemClick(View view, int position) {
-
-    }
-
-
-    View.OnClickListener onSend = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-
-
-            saveMessage();
-
-
-
-            messages.add(editText.getText().toString());
-            //userName.setText("Andy");
-            adapter.notifyDataSetChanged();
+                adapter.notifyDataSetChanged();
 
             editText.setText("");
 
 
-        }
-    };
+            }
+        });
+
+        return view;
+    }
+
+
+
+
+
+
+
+    //public void saveMessage(){
+    //    String message = editText.getText().toString();
+    //    ChatMessage infoMessage = new ChatMessage(message,"Ryan");
+    //    if (message.isEmpty()){return;}
+        //mDocRef.set(infoMessage);
+    //}
+
+    //@Override
+    //public void onItemClick(View view, int position) {
+
+    //}
+
+
+//    View.OnClickListener onSend = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//
+//
+//
+//            if(!editText.getText().toString().equals("")){
+//                messageSend.setMessageUser("FATHER");
+//                messageSend.setMessageText(editText.getText().toString());
+//
+//                String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+//                DateFormat time = new SimpleDateFormat("hh:mm:ss a");
+//                String formatTime = time.format(new Date());
+//                messageSend.setMessageTime(date + " " + formatTime);
+//
+//
+//                sendDataTodataBase();
+//            }else{
+//                Toast.makeText(getActivity(), "message can not be empty", Toast.LENGTH_SHORT).show();
+//            }
+//            //saveMessage();
+//
+//
+//
+//            //messages.add(editText.getText().toString());
+//            //userName.setText("Andy");
+//
+//            //adapter.notifyDataSetChanged();
+//
+//            editText.setText("");
+//
+//
+//        }
+//    };
+
+    public void getMessageFromDataBase(){
+        Task<QuerySnapshot> result =  db.collection("Test").get();
+        //Task<QuerySnapshot> result =  db.collection("Repair").get();
+        //Request all the documents in the data collection "communication"
+        result.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        //Log.d(TAG, document.getId() + " => " + document.getData());
+
+                        ChatMessage chatMessage = new ChatMessage();
+                        //Toast.makeText(getActivity(), document.get("messageText").toString(), Toast.LENGTH_SHORT).show();
+                        chatMessage.setMessageText(document.get("messageText").toString());
+                        chatMessage.setMessageTime(document.get("messageTime").toString());
+                        chatMessage.setMessageUser(document.get("messageUser").toString());
+                        ChatMessage tryMessage = new ChatMessage(document.get("messageText").toString(),document.get("messageUser").toString(),document.get("messageTime").toString());
+                        infMessages.add(tryMessage);
+//                        messageList.setAdapter(adapter);
+//                        adapter.notifyDataSetChanged();
+                    }
+
+
+                    adapter = new MessagrRecycleViewAdapter(getActivity(),infMessages);
+                    messageList.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+
+                }
+            }
+        });
+    }
+
+            private void sendDataTodataBase(){
+                Task<Void> result = messageSend.addValues();
+                result.addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(messageSend.isSuccessful()){
+                            Toast.makeText(getActivity(),"Message sent", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+                adapter.notifyDataSetChanged();
+                infMessages.clear();
+            }
 
 
 }
